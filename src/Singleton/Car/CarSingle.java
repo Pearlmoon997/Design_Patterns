cc4ed7dcbdaa6a25e968d08288d8acadf09d1faf
moongi <pearlmoon997@gmail.com>
2022-05-22 15:25:28 +0900

    Singleton Pattern_ver.02

---
+++ src/Singleton/Car/CarSingle.java
@@ -0,0 +1,31 @@
+package Singleton.Car;
+
+public class CarSingle {
+    private boolean Inuse;
+    private static CarSingle uniqueInstance;
+
+    private CarSingle() {
+        Inuse = false;
+    }
+
+    public static CarSingle getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new CarSingle();
+        }
+        return uniqueInstance;
+    }
+
+    public void drive(){
+            Inuse = true;
+            System.out.println("Driving");
+    }
+
+    public void cancel() {
+            Inuse = false;
+            System.out.println("Inuse, please wait");
+    }
+
+    public boolean isEnable() {
+        return !Inuse;
+    }
+}
---
+++ src/Singleton/Car/Starter.java
@@ -0,0 +1,19 @@
+package Singleton.Car;
+
+public class Starter {
+    public static void main(String[] args){
+        CarSingle car = CarSingle.getInstance();
+        if (car.isEnable()) {
+            car.drive();
+        } else {
+            System.out.println("Car is Inuse");
+        }
+
+        CarSingle car2 = CarSingle.getInstance();
+        if(!car2.isEnable()){
+            car2.cancel();
+        } else {
+            System.out.println("Can use");
+        }
+    }
+}
